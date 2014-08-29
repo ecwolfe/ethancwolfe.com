9939b93ae66ac56c4b1979ea35ffab74d770cc7f
Joel Wolfe <joelwwolfe@gmail.com>
2014-08-28 22:54:55 -0500

    Fixed boggle, added js, adjusted css

--- Boggle.py
+++ Boggle.py
@@ -4,122 +4,99 @@
 import sys
 import random
 
-usedDice = []
-dictionary = []
-solvedWords = []
-
-def clearGame():
-	usedDice = []
+class Boggle:
 	dictionary = []
 	solvedWords = []
+	boggleBoard = []
 
-def beenUsed(count):
-	for i in xrange(0,len(usedDice)):
-		if (count == usedDice[i]):
-			return True
-	return False
 
-def setupBoggle():
+	def __init__(self):
 
-	board = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+		# Notice the second dice has Q instead of Qu
+		dice =  ['FORIXB', 'MOQABJ', 'GURILW', 'SETUPL', 'CMPDAE', 'ACITAO', 'SLCRAE', 'ROMASH', 'NODESW', 'HEFIYE', 'ONUDTK', 'TEVIGN', 'ANEDVZ', 'PINESH', 'ABILYT', 'GKYLEU']
+		
+		results = [d[random.randrange(0,6)] for d in dice]
+		random.shuffle(results)
 
-	# Notice the second dice has Q instead of Qu
-	dice =  ['FORIXB', 'MOQABJ', 'GURILW', 'SETUPL', 'CMPDAE', 'ACITAO', 'SLCRAE', 'ROMASH', 'NODESW', 'HEFIYE', 'ONUDTK', 'TEVIGN', 'ANEDVZ', 'PINESH', 'ABILYT', 'GKYLEU']
+		self.boggleBoard = [['' for y in xrange(4)] for x in xrange(4)]
 
-	for i in xrange(0,16):
-		rDie = random.randrange(0,16)
-		rPos = random.randrange(0,16)
-
-		if i > 0:
-			while ((board[rPos] != '') | beenUsed(rDie)):
-				rDie = random.randrange(0,16)
-				rPos = random.randrange(0,16)
-
-		usedDice.append(rDie)
-
-		rFace = random.randrange(0,6)
-		board[rPos] = dice[rDie][rFace]
-
-		boggleBoard = [['' for y in xrange(4)] for x in xrange(4)]
-	
 		count = 0
 
 		for l in xrange(0,4):
 			for k in xrange(0,4):
-				boggleBoard[l][k] = board[count]
+				self.boggleBoard[l][k] = results[count]
 				count += 1
+		
+		
+	def getBoard(self):
+		return self.boggleBoard
 
-	return boggleBoard
+	def clearWordList(self):
+		self.solvedWords = []
+		words = []
 
-def getNeighbors(row, col):
-	neigh = []
-	for i in xrange(0,3):
-		for j in xrange(0,3):
-			newRow = (row - 1) + i
-			newCol = (col - 1) + j
-			if (newRow>0 and newRow<4 and newCol>0 and newCol<4):
-				neigh.append([newRow,newCol])
-	return neigh
+	def getNeighbors(self,row, col):
+		neigh = []
+		for i in xrange(0,3):
+			for j in xrange(0,3):
+				newRow = (row - 1) + i
+				newCol = (col - 1) + j
+				if (newRow>0 and newRow<4 and newCol>0 and newCol<4):
+					neigh.append([newRow,newCol])
+		return neigh
 
-def isWord(inWord):
-	left = 0
-	right = len(dictionary)
+	def isWord(self,inWord):
+		left = 0
+		right = len(self.dictionary)
 
-	while (left <= right):
-		middle = (left + right)/2
-		#print left
-		#print right
-		if( inWord > dictionary[middle] ):
-			left = middle + 1
-		elif( inWord < dictionary[middle] ):
-			right = middle - 1
-		else:
-			return True
+		while (left <= right):
+			middle = (left + right)/2
+			#print left
+			#print right
+			if( inWord > self.dictionary[middle] ):
+				left = middle + 1
+			elif( inWord < self.dictionary[middle] ):
+				right = middle - 1
+			else:
+				return True
 
-	return False
+		return False
 
-def wordSearch(x,y,testWord,boggleBoard):
-	BoggleBoard = boggleBoard
-	if(len(testWord)>2):
-		if(isWord(testWord)):
-			if testWord not in solvedWords:
-				solvedWords.append(testWord)
-	if(len(testWord)>7):  # MAX_WORD_LENGTH
-		return
-	testWord += BoggleBoard[x][y]
-	temp = BoggleBoard[x][y]
-	BoggleBoard[x][y] = ''
-	neighbors = getNeighbors(x,y)
-	#print neighbors
-	for p in xrange(0,len(neighbors)):
-		a = neighbors[p][0]
-		s = neighbors[p][1]
-		if (len(BoggleBoard[a][s])>0):
-			#print testWord
-			wordSearch(a,s,testWord,BoggleBoard)
-	BoggleBoard[x][y] = temp
+	def wordSearch(self,x,y,testWord):
 
-def printBoard(BoggleBoard):
-	for i in xrange(0,4):
-		print '\n',
-		for j in xrange(0,4):
-			print BoggleBoard[i][j],
-	print '\n',
-
+		if(len(testWord)>2):
+			if(self.isWord(testWord)):
+				if testWord not in self.solvedWords:
+					self.solvedWords.append(testWord)
+		if(len(testWord)>7):  # MAX_WORD_LENGTH
+			return
+		testWord += self.boggleBoard[x][y]
+		temp = self.boggleBoard[x][y]
+		self.boggleBoard[x][y] = ''
+		neighbors = self.getNeighbors(x,y)
+		#print neighbors
+		for p in xrange(0,len(neighbors)):
+			a = neighbors[p][0]
+			s = neighbors[p][1]
+			if (len(self.boggleBoard[a][s])>0):
+				#print testWord
+				self.wordSearch(a,s,testWord)
+		self.boggleBoard[x][y] = temp
 
 
-def playGame(BoggleBoard):
-	f = open ('sowpods.txt','r')
+	def playGame(self):
 
-	for line in f:
-		dictionary.append(line.rstrip('\r\n'))
+		f = open ('sowpods.txt','r')
 
-	f.close()
+		for line in f:
+			self.dictionary.append(line.rstrip('\r\n'))
 
-	for l in xrange(0,4):
-		for k in xrange(0,4):
-			wordSearch(l,k,'',BoggleBoard)
+		f.close()
 
-	words = solvedWords
-	
-	return words
+		for l in xrange(0,4):
+			for k in xrange(0,4):
+				self.wordSearch(l,k,'')
+
+		words = self.solvedWords
+		
+		return sorted(words)
